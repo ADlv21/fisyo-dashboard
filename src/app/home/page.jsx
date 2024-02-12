4b1461c9a8e09708f27af841da3b126a7ea793f6
Adwait Gawade <adwaitg02@gmail.com>
2024-02-12 16:00:27 +0530

    init

---
+++ src/app/home/page.jsx
@@ -0,0 +1,13 @@
+import Button from '@/components/Button'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div>Home Page</div>
+            <Button />
+        </>
+    )
+}
+
+export default page
